a01aca338bbfab097a3530a3fb44a432696f630b
lawson101 <lawsoncodes101@gmail.com>
2025-11-04 00:47:30 +0100

    created style.css

--- script.js
+++ script.js
@@ -2,11 +2,14 @@ const container = document.querySelector("#container");
 let elementsArray = [];
 
 for (let i = 1; i <= 16; i++) {
+    let row = document.createElement("div");
+    row.classList.add("row");
     for (let j = 1; j <= 16; j++) {
-        let newDiv = document.createElement("div");
-        newDiv.textContent = `I'm DIV number ${j}`;
-        elementsArray.push(newDiv);
+        let column = document.createElement("div");
+        column.textContent = `I'm column number ${j}`;
+        column.classList.add("column");
+        row.append(column);
     }
+    elementsArray.push(row);
 }
 container.append(...elementsArray);
-console.log(container.childElementCount);
