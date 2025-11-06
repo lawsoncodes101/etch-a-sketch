2875e84d37832446d30341eea201dac91349b479
lawson101 <lawsoncodes101@gmail.com>
2025-11-06 17:24:57 +0100

    Added control logic

--- script.js
+++ script.js
@@ -1,19 +1,46 @@
 const grid = document.querySelector("#grid");
+const colors = document.querySelectorAll(".color");
+const setButton = document.querySelector("form > button");
+const input = document.querySelector("input");
+let gridColor = "red";
 let gridSize = 16;
 let elementsArray = [];
 
-for (let i = 1; i <= gridSize; i++) {
-    let row = document.createElement("div");
-    row.classList.add("row");
-    for (let j = 1; j <= gridSize; j++) {
-        let column = document.createElement("div");
-        column.classList.add("column");
-        column.addEventListener("mouseover", (e) => {
-            e.target.style.background = "red";
-        })
-        row.append(column);
-    }
-    elementsArray.push(row);
-}
+colors.forEach((color) => {
+    color.addEventListener("click", () => {
+        gridColor = color.id;
+    });
+});
 
-grid.append(...elementsArray);
+setButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    let newSize = parseInt(input.value);
+    if (newSize > 1 && newSize <= 25) {
+        gridSize = newSize;
+        createGrid();
+    } else {
+        alert("Your grid must be between 2 to 25");
+    }
+});
+
+function createGrid() {
+    while (grid.firstChild) {
+        grid.removeChild(grid.firstChild);
+    }
+    elementsArray = [];
+    for (let i = 1; i <= gridSize; i++) {
+        let row = document.createElement("div");
+        row.classList.add("row");
+        for (let j = 1; j <= gridSize; j++) {
+            let column = document.createElement("div");
+            column.classList.add("column");
+            column.addEventListener("mouseover", (e) => {
+                e.target.style.background = `${gridColor}`;
+            });
+            row.append(column);
+        }
+        elementsArray.push(row);
+    }
+    grid.append(...elementsArray);
+}
+createGrid();
